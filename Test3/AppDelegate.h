cb94ae8dbeaab2233ff4c6207d986b1935ce9a47
Vitaly Matzkanyuk <High@vitalys-macbook-pro.local>
2015-10-05 19:58:13 +0300

    Initial Commit

---
+++ Test3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test3
+//
+//  Created by Vitaly Matzkanyuk on 05/10/15.
+//  Copyright © 2015 Vitaly Matzkanyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
